48b2e9b0e3f22e522a44b238e10742521a54102e
Oscar Gross Jr <oscar.gross222@gmail.com>
2020-10-03 18:44:47 -0300

    Insertion routes and creation model Product2

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const { response } = require('express');
 const express = require ('express')
-const mongoose = require('mongoose')
- 
+const mongoose = require('mongoose') 
 const app = express();
 app.use(express.json());
 
